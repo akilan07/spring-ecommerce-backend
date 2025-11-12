93bdbf762da0334f7be6e75c389bd3a4527f8117
akilan07 <akilan07r@gmail.com>
2025-11-12 13:51:16 +0530

    Remove unused imports

--- src/main/java/com/akisha/spring_ecommerce_backend/api/impl/HealthCheckController.java
+++ src/main/java/com/akisha/spring_ecommerce_backend/api/impl/HealthCheckController.java
@@ -1,10 +1,8 @@
 package com.akisha.spring_ecommerce_backend.api.impl;
 
 import com.akisha.spring_ecommerce_backend.api.HealthCheckApi;
-import io.swagger.v3.oas.annotations.Parameter;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.core.OAuth2AuthenticatedPrincipal;
 import org.springframework.web.bind.annotation.RestController;
