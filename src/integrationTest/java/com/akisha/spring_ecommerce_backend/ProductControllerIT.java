447833703eca884e747f440cb80b4f0a66e91321
akilan07 <akilan07r@gmail.com>
2025-12-28 09:10:01 +0530

    Refactor IT

--- src/integrationTest/java/com/akisha/spring_ecommerce_backend/ProductControllerIT.java
+++ src/integrationTest/java/com/akisha/spring_ecommerce_backend/ProductControllerIT.java
@@ -7,21 +7,25 @@ import com.akisha.spring_ecommerce_backend.config.MongoTestContainerConfig;
 import com.akisha.spring_ecommerce_backend.dto.Product;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.TestConstructor;
 import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
 @AutoConfigureMockMvc(addFilters = false)
+@TestConstructor(autowireMode = TestConstructor.AutowireMode.ALL)
 class ProductControllerIT extends MongoTestContainerConfig{
 
-    @Autowired
-    MockMvc mockMvc;
+    private final MockMvc mockMvc;
 
-    @Autowired
-    ObjectMapper objectMapper;
+    private final ObjectMapper objectMapper;
+
+    ProductControllerIT(MockMvc mockMvc, ObjectMapper objectMapper) {
+        this.mockMvc = mockMvc;
+        this.objectMapper = objectMapper;
+    }
 
     @Test
     void contextLoads() {
